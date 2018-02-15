34334a52bf6d0d5adf2e5445385a9060be7b1415
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-02-15 15:24:41 +0530

    popupmenu to the toolbar

--- 1Toolbar/app/src/main/java/com/example/arjun27/a1toolbar/MainActivity.java
+++ 1Toolbar/app/src/main/java/com/example/arjun27/a1toolbar/MainActivity.java
@@ -3,16 +3,39 @@ package com.example.arjun27.a1toolbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+
+        if(id==R.id.item1){
+            Toast.makeText(this, "item1 clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
